39a19cf9fb66c29ea3e792d773316bc83609a045
Vyacheslav <viacheslav.saiapin@gmail.com>
2023-01-30 17:56:25 +0300

    test observer

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,10 +4,20 @@ import "./SideBar.css";
 export default class SideBar {
   constructor(selectorName) {
     this.selectorName = selectorName;
+    this.currentCategoty = "pizza";
   }
 
   addEvent() {
     const allTabs = document.querySelectorAll(".side_bar__link");
+    for (let i = 0; i < allTabs.length; i++) {
+      allTabs[i].addEventListener("click", (e) => {
+        const currentChildren = e.target.parentElement.children;
+        for (let i = 0; i < currentChildren.length; i++) {
+          currentChildren[i].classList.remove("active_tab");
+        }
+        e.target.classList.add("active_tab");
+      });
+    }
   }
 
   render() {
@@ -16,7 +26,7 @@ export default class SideBar {
     let html = "";
 
     TAB_CATEGORIES.forEach(({ category, name }) => {
-      html += `<nav class="side_bar__link active_tab" id=${category}>${name}</nav>`;
+      html += `<nav class="side_bar__link" id=${category}>${name}</nav>`;
     });
 
     rootElement.innerHTML = html;
--- src/script.js
+++ src/script.js
@@ -1,7 +1,4 @@
-import data from "./assets/data.json";
 import "./style.css";
 import App from "./App";
 
-
 new App().render();
-
